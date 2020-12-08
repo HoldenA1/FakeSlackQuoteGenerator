8602e06a7669aeca4b1ccacff2ae9cd88da32e1a
holdena1 <holden.adamec@gmail.com>
2020-12-07 21:19:11 -0800

    Converted to PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+var cacheName = 'quotes-gen-pwa';
+var databaseName = 'quotes-app-data'
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/favicon.ico',
+    '/manifest.json',
+    '/images/favicon-96x96.png',
+    '/main.css'
+];
+
+// Start the service worker and cache all of the app's content
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        // Try the cache
+        caches.match(event.request).then(function(response) {
+            // Fall back to network
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Remove previous cached data from disk
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
